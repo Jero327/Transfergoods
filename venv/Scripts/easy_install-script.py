c863023e51466d4b87211c93c87a69cd5407fa02
Jero <jeraviah@gmail.com>
2020-03-25 22:59:42 +1300

    Delete poll app, add auth

--- venv/Scripts/easy_install-script.py
+++ venv/Scripts/easy_install-script.py
@@ -1,4 +1,4 @@
-#!E:\Python\practice\untitled7420\venv\Scripts\python.exe
+#!E:\back-end\Python\practice\django_auth_example\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install'
 __requires__ = 'setuptools==40.8.0'
 import re
--- venv/Scripts/pip3-script.py
+++ venv/Scripts/pip3-script.py
@@ -1,4 +1,4 @@
-#!E:\Python\practice\untitled7420\venv\Scripts\python.exe
+#!E:\back-end\Python\practice\django_auth_example\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3'
 __requires__ = 'pip==19.0.3'
 import re
