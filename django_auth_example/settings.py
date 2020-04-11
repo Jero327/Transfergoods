4711d08a15977b263cf4d9907d79f3f2d0061941
Jero <jeraviah@gmail.com>
2020-04-11 19:06:29 +1200

    Extend shell_plus.

--- django_auth_example/settings.py
+++ django_auth_example/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shell_plus',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
