26dac0b2c7c4832d858c7aeee7242b635945c010
Jero <jeraviah@gmail.com>
2020-04-15 14:46:23 +1200

    ALLOWED_HOSTS: Localhost, https://transfergoods.herokuapp.com/

--- django_auth_example/settings.py
+++ django_auth_example/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = '0s(bwn9^r$fqulu+cq)(gzc%@s84i*-2azqtdeyy)af^w3-a^w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['transfergoods.herokuapp.com']
+ALLOWED_HOSTS = [
+    'transfergoods.herokuapp.com',
+    '127.0.0.1',
+                 ]
 
 
 # Application definition
