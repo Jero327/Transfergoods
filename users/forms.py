9addffa670e30645898bb8d6f8af2a9db0754b56
Jero <jeraviah@gmail.com>
2020-04-20 22:39:09 +1200

    When register: User must input a email address, which can not be same with a existed one.

--- users/forms.py
+++ users/forms.py
@@ -5,11 +5,25 @@ import datetime
 from .models import *
 from PIL import Image
 from io import StringIO
+from django.core.exceptions import ValidationError
 
 class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True, label='Email address', error_messages={'exists': 'Oops, this email address already exists.'})
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ("username", "email")
+
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
+    def clean_email(self):
+        if User.objects.filter(email=self.cleaned_data['email']).exists():
+            raise ValidationError(self.fields['email'].error_messages['exists'])
+        return self.cleaned_data['email']
 
 class AddServiceForm(forms.ModelForm):
 
