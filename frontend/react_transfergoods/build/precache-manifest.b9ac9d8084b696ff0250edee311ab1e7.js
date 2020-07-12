322a665951db5d4152b714c15b42e297c5dc69dc
Jero <jeraviah@gmail.com>
2020-07-13 00:51:05 +1200

    Add react build/

---
+++ frontend/react_transfergoods/build/precache-manifest.b9ac9d8084b696ff0250edee311ab1e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ce00686ce29e8c1857039336cdfcdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03c34d6ea30e938dab80",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1e6628f93922d3fe93ee",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "03c34d6ea30e938dab80",
+    "url": "/static/js/2.b8b6dd50.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.b8b6dd50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6628f93922d3fe93ee",
+    "url": "/static/js/main.fd3b321b.chunk.js"
+  },
+  {
+    "revision": "3dc74105370746b6300a",
+    "url": "/static/js/runtime-main.9511349f.js"
+  }
+]);
